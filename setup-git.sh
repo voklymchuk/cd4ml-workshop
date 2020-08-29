66546817403aed7f1b450164a6467df565b70845
Valerii Klymchuk <voklymchuk@gmail.com>
2020-08-29 15:30:28 -0400

    Sample change

--- setup-git.sh
+++ setup-git.sh
@@ -6,7 +6,8 @@ read -p 'Github username: ' GITHUB_USER
 read -sp 'Github personal access token: ' GITHUB_TOKEN
 echo
 echo "Setting up git..."
+export
 git config --global user.name "$NAME"
 git config --global user.email "$EMAIL"
-git remote set-url origin https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_USER/cd4ml-workshop
+git remote set-url origin https://$GITHUB_USER:29dfecc9ed632cb6bfd62c3dc244f36dbcca3dbb@github.com/$GITHUB_USER/cd4ml-workshop
 git pull --rebase
